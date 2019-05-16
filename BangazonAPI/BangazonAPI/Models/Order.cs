3b2c3d76b8ee6e81f80e6165d37cd2142ddbfa37
MattRowee <29.matt@gmail.com>
2019-05-16 13:40:03 -0400

    seeded models and altered appsettings.json

---
+++ BangazonAPI/BangazonAPI/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Order
+    {
+        public int id { get; set; }
+        public List<Customer> customer = new List<Customer>();
+        public Customer getCustomerById(int id)
+        {
+            return customer[id];
+        }
+        public List<PaymentType> paymentType = new List<PaymentType>();
+        public PaymentType getPTypeById(int id)
+        {
+            return paymentType[id];
+        }
+    }
+}
---
+++ BangazonAPI/BangazonAPI/Models/OrderProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class OrderProduct
+    {
+        public int id { get; set; }
+        public List<Product> product = new List<Product>();
+        public Product getProductById(int id)
+        {
+            return product[id];
+        }
+        public List<Order> order = new List<Order>();
+        public Order getOrderById(int id)
+        {
+            return order[id];
+        }
+    }
+}
